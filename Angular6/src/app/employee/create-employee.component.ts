13ae63e98aca598cdb5a025e71def746afe000cf
Varathan <varathansuyambu95@gmail.com>
2019-03-20 23:04:55 +0530

    validation messages moved to ts file

--- Angular6/src/app/employee/create-employee.component.ts
+++ Angular6/src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,FormBuilder,Validators} from '@angular/forms'
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-create-employee',
@@ -8,8 +8,35 @@ import { FormGroup, FormControl,FormBuilder,Validators} from '@angular/forms'
 })
 export class CreateEmployeeComponent implements OnInit {
   employeeForm: FormGroup;
-  fullNameLength=0;
-  constructor(private fb:FormBuilder) { }
+  fullNameLength = 0;
+  validationMessages = {
+    'fullName': {
+      'required': 'Full Name is required.',
+      'minlength': 'Full Name must be greater than 2 characters.',
+      'maxlength': 'Full Name must be less than 10 characters.'
+    },
+    'email': {
+      'required': 'Email is required.'
+    },
+    'skillName': {
+      'required': 'Skill Name is required.',
+    },
+    'experienceInYears': {
+      'required': 'Experience is required.',
+    },
+    'proficiency': {
+      'required': 'Proficiency is required.',
+    },
+  };
+  formErrors = {
+    'fullName': '',
+    'email': '',
+    'skillName': '',
+    'experienceInYears': '',
+    'proficiency': ''
+  };
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     // this.employeeForm = new FormGroup({
@@ -21,31 +48,65 @@ export class CreateEmployeeComponent implements OnInit {
     //     proficiency :new FormControl()
     //   })
     // });
-    this.employeeForm=this.fb.group({
-      fullName:['',[Validators.required,Validators.minLength(4),Validators.maxLength(15)]],
-      email:[''],
-      skills:this.fb.group({
-        skillName:[''],
-        experienceInYears:[''],
-        proficiency:['']
+    this.employeeForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(15)]],
+      email: ['', Validators.required],
+      skills: this.fb.group({
+        skillName: ['', Validators.required],
+        experienceInYears: ['', Validators.required],
+        proficiency: ['', Validators.required]
       })
     });
-    this.employeeForm.get('fullName').valueChanges.subscribe((value:string)=>{
-      this.fullNameLength=value.length;
-    console.log(value);
+    this.employeeForm.get('fullName').valueChanges.subscribe((value: string) => {
+      this.fullNameLength = value.length;
+      console.log(value);
     });
   }
-  onLoadDataClick():void{
-    //patchValue for partial upadte
-    this.employeeForm.setValue({
-      fullName:'Varathan',
-      email: 'varathan@gmail.com',
-      skills :{
-        skillName: 'Angular 2',
-        experienceInYears: '3',
-        proficiency :'intermediate'
+  logKeyValuePair(group: FormGroup): void {
+    Object.keys(group.controls).forEach((key: string) => {
+      const abstractControl = group.get(key);
+      if (abstractControl instanceof FormGroup) {
+        this.logKeyValuePair(abstractControl);
+      }
+      else {
+        console.log('Key = ' + key + ' Value = ' + abstractControl.value);
       }
     });
+
+  }
+  logValidationError(group: FormGroup): void {
+    Object.keys(group.controls).forEach((key: string) => {
+      const abstractControl = group.get(key);
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationError(abstractControl);
+      }
+      else {
+        this.formErrors[key]="";
+        if (abstractControl && !abstractControl.valid) {
+          const messages = this.validationMessages[key];
+          for (const errorKey in abstractControl.errors) {
+            if (errorKey) {
+              this.formErrors[key] += messages[errorKey] + ' ';
+            }
+          }
+        }
+      }
+    });
+  }
+  onLoadDataClick(): void {
+    //patchValue for partial upadte
+    // this.employeeForm.setValue({
+    //   fullName: 'Varathan',
+    //   email: 'varathan@gmail.com',
+    //   skills: {
+    //     skillName: 'Angular 2',
+    //     experienceInYears: '3',
+    //     proficiency: 'intermediate'
+    //   }
+    // });
+    //this.logKeyValuePair(this.employeeForm);
+    this.logValidationError(this.employeeForm);
+    console.log(this.formErrors);
   }
   onSubmit(): void {
     console.log(this.employeeForm.value);
