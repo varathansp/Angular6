abcc828d31a6befffe34162d3767d4c6bc9ca34f
Varathan <varathansuyambu95@gmail.com>
2019-03-20 14:06:55 +0530

    Forms added using formbuilder class

--- Angular6/src/app/employee/create-employee.component.ts
+++ Angular6/src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms'
+import { FormGroup, FormControl,FormBuilder,Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-create-employee',
@@ -8,12 +8,43 @@ import { FormGroup, FormControl } from '@angular/forms'
 })
 export class CreateEmployeeComponent implements OnInit {
   employeeForm: FormGroup;
-  constructor() { }
+  fullNameLength=0;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
-    this.employeeForm = new FormGroup({
-      fullName: new FormControl(),
-      email: new FormControl()
+    // this.employeeForm = new FormGroup({
+    //   fullName: new FormControl(),
+    //   email: new FormControl(),
+    //   skills :new FormGroup({
+    //     skillName: new FormControl(),
+    //     experienceInYears: new FormControl(),
+    //     proficiency :new FormControl()
+    //   })
+    // });
+    this.employeeForm=this.fb.group({
+      fullName:['',[Validators.required,Validators.minLength(4),Validators.maxLength(15)]],
+      email:[''],
+      skills:this.fb.group({
+        skillName:[''],
+        experienceInYears:[''],
+        proficiency:['']
+      })
+    });
+    this.employeeForm.get('fullName').valueChanges.subscribe((value:string)=>{
+      this.fullNameLength=value.length;
+    console.log(value);
+    });
+  }
+  onLoadDataClick():void{
+    //patchValue for partial upadte
+    this.employeeForm.setValue({
+      fullName:'Varathan',
+      email: 'varathan@gmail.com',
+      skills :{
+        skillName: 'Angular 2',
+        experienceInYears: '3',
+        proficiency :'intermediate'
+      }
     });
   }
   onSubmit(): void {
